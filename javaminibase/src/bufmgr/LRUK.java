dd36418e5e956fffb2e803a621d632f129ed92ec
boukhary123 <alaaboukhary@gmail.com>
2018-11-01 04:26:23 -0700

    Completed Code without trying it

--- javaminibase/src/bufmgr/LRUK.java
+++ javaminibase/src/bufmgr/LRUK.java
@@ -8,11 +8,65 @@ import java.lang.*;
 
 
 
+class history{
 
-  /**
-   * class LRU is a subclass of class Replacer using LRU
-   * algorithm for page replacement
-   */
+  private Map<Integer, Long[]> Hist;
+  private Map<Integer, Long> Last;
+  public static final int Correlated_Reference_Period=100000;
+  private int length;
+
+  public history(){
+      Map<Integer, Long[]> Hist = new HashMap<Integer, Long[]>();
+      Map<Integer, Long[]> Last = new HashMap<Integer, Long>();
+  }
+  public int get_k_access(int p, int k){
+    return Hist.get(p)[k];
+  }
+
+  public int get_last_access(int p){
+    return Last.get(p);
+  }
+  public void set_k_access(int p,int k,Long val){
+    Long tmp[];
+    tmp=Hist.get(p);
+    tmp[k]=val;
+    Hist.put(p,tmp);
+  }
+  public void update_last_access(int p){
+    Last.put(p,System.currentTimeMillis());
+  }
+
+  public void update_last_access_hist(int p){
+    Hist.put(p,System.currentTimeMillis());
+  }
+  public void is_page_present(int p){
+    // check if a paricular page is present in history
+    return Hist.containsKey(p);
+  }
+  public void updating_hist(int p){
+    Long tmp[];
+    tmp=Hist.get(p);
+    for(int i=1;i<length;i++){
+      tmp[i]=tmp[i-1];
+    }
+    Hist.put(p,tmp);
+  }
+  public void updating_hist_const(int p, Long constant){
+    Long tmp[];
+    tmp=Hist.get(p);
+    for(int i=1;i<length;i++){
+      tmp[i]=tmp[i-1]+constant;
+    }
+    Hist.put(p,tmp);
+  }
+  public void allocate_block(int p){
+    Long tmp[];
+    tmp = new Long[length];
+    Hist.put(p,tmp);
+  }
+}
+
+
 
 class LRUK extends  Replacer {
 
@@ -22,13 +76,20 @@ class LRUK extends  Replacer {
    */
 
     private int  frames[];
- 
+
   /**
    * private field
    * number of frames used
-   */   
+   */
   private int  nframes;
   private int lastRef;
+  // varibale that stores hist and last data structures
+  private history hist;
+  // variable used to track page id that is calling pin or
+  // pick_victim
+  public int pageid;
+
+  public int already_in_buffer=0;
 
 
   /**
@@ -37,14 +98,47 @@ class LRUK extends  Replacer {
    */
   private void update(int frameNo)
   {
-     int index;
-     for ( index=0; index < nframes; ++index )
-        if ( frames[index] == frameNo )
-            break;
+    //  int index;
+    //  for ( index=0; index < nframes; ++index )
+    //     if ( frames[index] == frameNo )
+    //         break;
+    //
+    // while ( ++index < nframes )
+    //     frames[index-1] = frames[index];
+    //     frames[nframes-1] = frameNo;
+    if (already_in_buffer==0){
+      // page not in buffer
+    if (HIST.is_page_present(pageid)){
+      // page is present in history
+      HIST.updating_hist(pageid);
+    }
+    else{
+      HIST.allocate_block(pageid);
+    }
+    HIST.update_last_access(p);
+    HIST.update_last_access_hist(p);
+  }
 
-    while ( ++index < nframes )
-        frames[index-1] = frames[index];
-        frames[nframes-1] = frameNo;
+  else{
+    // page in buffer
+    // update history information of p
+    Long t = System.currentTimeMillis();
+    int p = frames[frameNo];
+    Long correl_period_of_refd_page = 0;
+    if((t-HIST.get_last_access(p)> HIST.Correlated_Reference_Period)){
+      // new uncorrelated reference
+      correl_period_of_refd_page = HIST.get_last_access(p)-HIST.get_k_access(p,0);
+      HIST.updating_hist_const(p,correl_period_of_refd_page);
+      HIST.update_last_access(p);
+      HIST.update_last_access_hist(p);
+
+    }
+    else{
+      // correlated reference
+      HIST.update_last_access(p);
+    }
+
+  }
   }
 
   /**
@@ -60,8 +154,13 @@ class LRUK extends  Replacer {
     public void setBufferManager( BufMgr mgr )
      {
         super.setBufferManager(mgr);
-	frames = new int [ mgr.getNumBuffers() ];
-	nframes = 0;
+	      frames = new int [ mgr.getNumBuffers() ];
+	      nframes = 0;
+        pageid=-1;
+        already_in_buffer=-1;
+        lastRef=k;
+        hist= new history();
+
      }
 
 /* public methods */
@@ -74,13 +173,13 @@ class LRUK extends  Replacer {
     {
       super(mgrArg);
       frames = null;
-      lastRef=k;      
+
     }
-  
+
   /**
    * calll super class the same method
-   * pin the page in the given frame number 
-   * move the page to the end of list  
+   * pin the page in the given frame number
+   * move the page to the end of list
    *
    * @param	 frameNo	 the frame number to pin
    * @exception  InvalidFrameNumberException
@@ -88,9 +187,8 @@ class LRUK extends  Replacer {
  public void pin(int frameNo) throws InvalidFrameNumberException
  {
     super.pin(frameNo);
-
     update(frameNo);
-    
+
  }
 
   /**
@@ -100,59 +198,70 @@ class LRUK extends  Replacer {
    * @return 	return the frame number
    *		return -1 if failed
    */
-
  public int pick_victim()
  {
    int numBuffers = mgr.getNumBuffers();
    int frame;
-   
+
     if ( nframes < numBuffers ) {
+      // buffer is not full
+
+
         frame = nframes++;
-        frames[frame] = frame;
+        frames[frame] = pageid;
         state_bit[frame].state = Pinned;
         (mgr.frameTable())[frame].pin();
+        update(0);
         return frame;
     }
-
+    // buffer is full
+    Long min = System.currentTimeMillis();
+    Long t = System.currentTimeMillis();
+    int victim;
     for ( int i = 0; i < numBuffers; ++i ) {
-         frame = frames[i];
+         q = frames[i];
         if ( state_bit[frame].state != Pinned ) {
+
+          if((t-HIST.get_last_access(q))>
+          HIST.Correlated_Reference_Period & HIST.get_k_access(q,0) < min){
+          victim = q;
+          frame = i;
+          min =  HIST.get_k_access(q,0);
+
+          }
             state_bit[frame].state = Pinned;
             (mgr.frameTable())[frame].pin();
-            update(frame);
+            update(0);
             return frame;
         }
     }
 
     return -1;
  }
- 
+
   /**
    * get the page replacement policy name
    *
    * @return	return the name of replacement policy used
-   */  
-    public String name() { return "LRU"; }
- 
+   */
+    public String name() { return "LRUK"; }
+
   /**
    * print out the information of frame usage
-   */  
+   */
  public void info()
  {
     super.info();
 
-    System.out.print( "LRU REPLACEMENT");
-    
+    System.out.print( "LRUK REPLACEMENT");
+
     for (int i = 0; i < nframes; i++) {
         if (i % 5 == 0)
 	System.out.println( );
 	System.out.print( "\t" + frames[i]);
-        
+
     }
     System.out.println();
  }
-  
+
 }
-
-
-
